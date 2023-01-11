fef53c065ebf16eaafeb3be04581283066b58f5f
Michael Glass <me@mike.is>
2023-01-11 20:11:50 +0100

    reader uses read (#24)
    * reader uses read
    
    * Ensure protocolReader handles multiple messages in a single read
    
    Fixes a couple bugs in `protocolReader`:
    
    - A single `data` read can contain multiple messages; we must be sure to
      recurse and parse them out if this is so, since another event might
      not come by.
    - When reading a message into a JSON string that we then parse, we need
      to make sure only to read the size of the buffer indicated by
      `messageSize`.
    
    * Bump to 0.1.0-alpha.11
    
    Co-authored-by: Ayaz Hafiz <ayaz@rwx.com>

--- src/protocol.ts
+++ src/protocol.ts
@@ -102,41 +102,19 @@ export async function protocolRead(stream: Readable, { debug } = { debug: false 
 }
 
 /**
- * Reads messages using the standard 4-byte header protocol.
- *
- * Combining this with the `protocolRead` method will have unexpected consequences
+ * Attaches to the `data` handler on a stream, continually reading messages using the standard 4-byte header protocol.
  *
  * When a new message is received, `handler` is called.
+ *
+ * NOTE: This should not be used in combination with `protocolRead`, as both
+ * methods take over the `data` event.
  */
-export function protocolReader(stream: Readable, handler: (message: AbqTypes.InitMessage | AbqTypes.TestCaseMessage) => Promise<void>, { debug } = { debug: false }) {
-  let buffer = Buffer.from('')
-  let messageSize: number | undefined
-
-  stream.on('data', async chunk => {
-    if (debug) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      console.log('READER:', `Received chunk: ${chunk.toString()}`)
-    }
-
-    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    buffer = Buffer.concat([buffer, chunk], buffer.length + chunk.length)
-    if (buffer.length >= 4) {
-      messageSize = buffer.readUInt32BE(0)
-      buffer = buffer.subarray(4)
-    }
-    if (messageSize && buffer.length >= messageSize) {
-      // We now know the whole message is available; get it.
-      const currentMessage = buffer.toString('utf8')
-
-      // There might be an additional message waiting for us behind the one we
-      // just parsed. Reset the buffer to this new message.
-      buffer = buffer.subarray(messageSize)
-
-      await handler(JSON.parse(currentMessage))
-    } else if (debug) {
-      console.log('READER:', 'Incomplete chunk, waiting for next chunk')
-    }
-  })
+export async function protocolReader(stream: Readable, handler: (message: AbqTypes.InitMessage | AbqTypes.TestCaseMessage) => Promise<void>, { debug } = { debug: false }) {
+  let message = await protocolRead(stream, { debug })
+  while (message) {
+    await handler(message)
+    message = await protocolRead(stream, { debug })
+  }
 }
 
 const CURRENT_PROTOCOL_VERSION_MAJOR = 0
